f4e897f74a63d8158c46ab629ada3bab2bc171a2
Gabriel <hotelmenaplaza@gmail.com>
2024-12-16 13:39:23 +0100

    updated

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,7 @@ config = rx.Config(
     app_name="mena_cumples",
     cors_allowed_origins=[
         "http://localhost:3000",
-        "https://mena-cumples.vercel.app"
+        "https://mena-cumples.vercel.app",
+        "https://mena-cumples-kp2nw8r1s-menadevwebs-projects.vercel.app"
     ]
 )
