f0da74a362c1c031cf92d6268b199df3b38b388a
Gabriel <hotelmenaplaza@gmail.com>
2024-12-16 13:30:03 +0100

    ultima actualizacion backend

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,6 @@ config = rx.Config(
     app_name="mena_cumples",
     cors_allowed_origins=[
         "http://localhost:3000",
-        "https://mena-cumples.vercel.app/"
+        "https://mena-cumples.vercel.app"
     ]
 )
