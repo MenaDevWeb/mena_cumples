f8878ddeb21ea01ea241e8da5c447ad13aec0dcf
Gabriel <hotelmenaplaza@gmail.com>
2024-12-16 13:15:56 +0100

    backend updated

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="mena_cumples",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://mena-cumples.vercel.app/"
+    ]
 )
